f27e120ec3d14d2dc1cef80b14ce3be2e94a76e3
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-08-25 19:57:13 +0200

    add flex banner

--- components/homepage-content.tsx
+++ components/homepage-content.tsx
@@ -15,6 +15,20 @@ const HomePageContent = () => {
                         </Link>
                 </div>
             </div>
+                <div className="flex flex-col min-[830px]:flex-row w-full h-[500px] gap-x-8">
+                    <img src="./assets/images/homepage-content/starfinder-s01.png" className="flex-1 w-full max-[830px]:h-[400px] min-[830px]:w-[50%] object-cover animate-fadeInLeft"/>
+                    <div className="flex-1 flex flex-col items-center text-center place-content-center animate-fadeInRight break-words">
+                        <h2 className="font-secondary text-5xl">Starfinder S01</h2>
+                    <p>The perfect blend of tradition and performance for a sporty-elegant style.</p>
+                    <button>
+                        <Link href={"/"}>
+                            <button className="font-primaryb mt-4 lg:text-md xl:text-xl text-secondary bg-primary py-2 px-8 hover:bg-hov text-center place-self-center">
+                                Discover
+                            </button>
+                        </Link>
+                    </button>
+                    </div>
+                </div>
         </div>
     );
 };
