9f1565a66d2c6c876ac1101f1c95446b90aad4e5
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-08-27 19:22:12 +0200

    add rolling-watches asset

--- components/homepage-content.tsx
+++ components/homepage-content.tsx
@@ -39,6 +39,16 @@ const HomePageContent = () => {
                         </button>
                     </div>
                 </div>
+                <div>
+                    <video
+                        muted 
+                        autoPlay 
+                        loop 
+                        className="w-full h-[450px] rounded-b-[15%] min-[500px]:h-[500px] min-[500px]:rounded-b-[300px] md:rounded-b-[400px]
+                        lg:rounded-b-[400px] lg:w-[1024px] xl:h-[600px] xl:rounded-b-full xl:w-full min-[1470px]:h-[620px] min-[1600px]:h-[700px] min-[1900px]:h-[850px] object-cover animate-fadeIn animate-slower">
+                            <source src="./assets/images/homepage-content/rolling-watches.webm" type="video/webm" aria-label="Some watches" />
+                    </video>
+                </div>
         </div>
     );
 };
