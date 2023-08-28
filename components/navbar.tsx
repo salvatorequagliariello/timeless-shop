5872e44bdd4d98c6da9f5313ec311ea68ccaa8c5
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-08-28 17:49:46 +0200

    add cart logic

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,11 +5,13 @@ import Link from "next/link";
 import MainNav from "@/components/main-nav";
 import { ShoppingBag } from 'lucide-react';
 import getCategories from "@/actions/get-categories";
+import { cn } from "@/lib/utils";
 
 export const revalidate = 0;
 
 const Navbar = async () => {
     const categories = await getCategories();
+    const cartItems = 0;
 
     return (
             <div className="fixed w-full bg-secondary py-4 md:py-7 text-primary font-helvetica z-20">
@@ -18,9 +20,18 @@ const Navbar = async () => {
                         <Link href="/" className="flex-1">
                             <Image src={timelessLogo} width={170} alt="Timeless Company Logo" className="dark:filter dark:invert dark:grayscale"/>
                         </Link>
-                        <div className="flex flex-row-reverse md:flex-row gap-4 md:gap-12">
+                        <div className="flex flex-row-reverse md:flex-row gap-4 md:gap-12 items-center">
                             <MainNav data={categories}/>
-                            <ShoppingBag className="stroke-primary hover:stroke-hov cursor-pointer" />
+                            <button>
+                                <div className="relative flex items-end gap-x-1">
+                                    <ShoppingBag size={20} className={cn("stroke-primary hover:stroke-hov cursor-pointer")} />
+                                    <span 
+                                    className={cn("-top-2 -right-[7px] text-[12px] text-slate-50 bg-blue-800 font-primary px-1 py-[0.5px]", 
+                                    cartItems >= 1 ? "absolute" : "hidden")}>
+                                        {cartItems}
+                                    </span>
+                                </div>
+                            </button>
                         </div>
                     </div>
                 </Container>
