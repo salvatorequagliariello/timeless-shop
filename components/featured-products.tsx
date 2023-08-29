4a7a0d2e607eb376f46cbbcb00ec8184275c31e9
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-08-29 10:11:11 +0200

    fix query filtering

--- components/featured-products.tsx
+++ components/featured-products.tsx
@@ -8,12 +8,19 @@ interface FeaturedProductsProps {
 const FeaturedProducts: React.FC<FeaturedProductsProps> = ({
     items
 }) => {
+
+    console.log(items.length)
+
     return (
         <div>
             <h3>Featured Products</h3>
             {items.length === 0 && <NoResults />}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {items.map((item) )}
+                {items.map((item) => (
+                    <div key={item.id}>
+                        {item.name}
+                    </div>
+                ))}
             </div>
         </div>
     );
