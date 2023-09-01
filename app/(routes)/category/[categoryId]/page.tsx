049fc05d6becddef47965e1d4beb733404eee18d
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-09-01 10:03:49 +0200

    add category description and fix category header

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -29,18 +29,26 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
     const category = await getCategory(params.categoryId);
 
     return (
-        <div className="min-[1950px]:w-full max-w-[1920px] pt-24 mx-auto flex flex-col flex-wrap gap-y-4 text-primary font-primaryb">
-            <div className="flex overflow-hidden">
-                <h1 className="-bottom-4 left-0 font-secondary text-9xl place-self-end">
-                    {category.name} Collection
-                </h1>
-                <img src={category.billboard.imageUrl} className="rounded-l-full ml-auto lg:h-[700px]"/>
-            </div>
-            <div className="w-[85%]">
-
+        <div className="max-w-[1920px] pt-24 mx-auto flex flex-col gap-y-4 text-primary font-primaryb">
+            <div className="w-full max-w-[1920px] flex flex-col xl:flex-row h-[600px]">
+                <div className="w-2/5 place-self-end">
+                    <h1 className="font-secondary text-9xl h-full w-full">
+                        {category.name} Collection
+                    </h1>
+                </div>
+                <div className="w-3/5 h-full">
+                    <img src={category.billboard.imageUrl} className="w-full h-full rounded-l-full object-cover" />
+                </div>
             </div>
             <hr />
-            <Filter />
+            <div className="w-[85%] min-[1921px]:w-full mx-auto">
+                <div>
+                    <p  className="text-center">
+                        Founded in 1832, the famous Swiss watch brand Timeless can boast a long tradition characterised by the elegance and performance of its products. Those values are perfectly represented by the Planetocean Collection. This line is dedicated to men and women looking for a high performance timepiece that combines technical innovation and elegance.
+                    </p>
+                </div>
+            <hr className="my-4"/>
+            </div>
         </div>
     );
 };
