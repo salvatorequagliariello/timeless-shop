2fc4bdc9b7d540d492267f46ae38d4b5f407e528
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-09-01 17:27:30 +0200

    fix category filter logic

--- app/(routes)/watches/page.tsx
+++ app/(routes)/watches/page.tsx
@@ -4,6 +4,10 @@ import Filter from "@/components/filter";
 import CategoryDescription from "@/components/category-description";
 import getCases from "@/actions/get-cases";
 import ProductsList from "@/components/products-list";
+import getMovements from "@/actions/get-movements";
+import getBracelets from "@/actions/get-bracelets";
+import getCategories from "@/actions/get-categories";
+import NoResults from "@/components/no-results";
 
 export const revalidate = 0;
 
@@ -15,6 +19,7 @@ interface AllWatchesPageProps {
         caseId: string;
         braceletId: string;
         movementId: string;
+        categoryId: string;
     }
 }
 
@@ -23,14 +28,16 @@ const AllWatchesPage: React.FC<AllWatchesPageProps> = async ({
     searchParams
 }) => {
     const products = await getProducts({ 
-        categoryId: params.categoryId,
+        categoryId: searchParams.categoryId,
         caseId: searchParams.caseId,
         braceletId: searchParams.braceletId,
         movementId: searchParams.movementId
     });
 
     const cases = await getCases();
-    const category = await getCategory(params.categoryId);
+    const movements = await getMovements();
+    const bracelets = await getBracelets();
+    const categories = await getCategories();
 
     return (
         <div className="max-w-[1920px] pt-24 mx-auto flex flex-col gap-y-4 text-primary font-primaryb">
@@ -44,22 +51,23 @@ const AllWatchesPage: React.FC<AllWatchesPageProps> = async ({
                     <img src="./assets/images/all-watches-pic.webp" className="w-full h-full rounded-l-full object-cover self-start xl:self-center" />
                 </div>
             </div>
-            <hr />
-            <div className="w-[85%] min-[1921px]:w-full mx-auto">
+            <div className="w-[85%] min-[1921px]:w-full mx-auto mt-4">
                 <div>
                     <CategoryDescription category={"All watches"} />
                 </div>
-                <hr className="my-4"/>
-                <div className="flex justify-between">
-                    <h2 className="flex-1/4">
-
+                <hr className="mt-8"/>
+                <div className="flex justify-between my-4">
+                    <h2>
+                        FILTER YOUR SEARCH
                     </h2>
-                    <div className="flex-3/4 flex justify-between">
+                    <div className="w-max flex ml-auto gap-x-16">
+                        <Filter valueKey="categoryId" name="Collections" data={categories} />
                         <Filter valueKey="caseId" name="Case Material" data={cases} />
-                        <Filter valueKey="movementId" name="Movement" data={cases} />
-                        <Filter valueKey="caseId" name="Bracelet Material" data={cases} />
+                        <Filter valueKey="movementId" name="Movement" data={movements} />
+                        <Filter valueKey="braceletId" name="Bracelet Material" data={bracelets} />
                     </div>
                 </div>
+                <hr />
                 <div>
                     <ProductsList items={products} />
                 </div>
