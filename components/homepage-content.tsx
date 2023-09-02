8bae62a0c6f41867c8d9e7ed16396b3266aa20a2
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-09-02 09:28:26 +0200

    fix homepage content links

--- components/homepage-content.tsx
+++ components/homepage-content.tsx
@@ -13,7 +13,7 @@ const HomePageContent = async () => {
                 <img src={"./assets/images/homepage-content/moonwalker-banner.webp"} alt="A watch" className="h-[300px] lg:h-[400px] w-full object-cover"/>
                 <div className="absolute bottom-8 left-4 flex flex-col gap-y-4">
                     <h2 className="font-secondary text-slate-50 text-4xl lg:text-6xl px-2">The new Moonwalker Collection.</h2>
-                        <Link href={"/"} className="w-min text-xl px-2 lg:text-2xl text-zinc-900 font-primary flex items-center transition gap-x-2 hover:text-zinc-100">
+                        <Link href="/category/6205b908-c527-4c50-a6ce-beddc4947109" className="w-min text-xl px-2 lg:text-2xl text-zinc-900 font-primary flex items-center transition gap-x-2 hover:text-zinc-100">
                             Discover
                             <ArrowRight />
                         </Link>
@@ -26,7 +26,7 @@ const HomePageContent = async () => {
                     <h2 className="font-secondary text-primary text-3xl lg:text-5xl">Starfinder S01</h2>
                     <p className="text-primary">The perfect blend of tradition and performance for a sporty-elegant style.</p>
                     <button className="font-primaryb mt-4 text-md xl:text-xl text-secondary bg-primary py-2 px-8 hover:bg-hov text-center place-self-center transition">
-                        <Link href={"/"}>
+                        <Link href={"/product/501b894b-489f-4486-8353-e613f1dfe93e"}>
                                 Discover
                         </Link>
                     </button>
@@ -38,7 +38,7 @@ const HomePageContent = async () => {
                     <h2 className="font-secondary text-primary text-3xl lg:text-5xl">Planetocean P97</h2>
                     <p className="text-primary lg:w-[70%]">Inspired by the unique requirements of this sport, Planetocean timepieces preserve the traditional style of diving watches.</p>
                     <button className="font-primaryb mt-4 text-md xl:text-xl text-secondary bg-primary py-2 px-8 hover:bg-hov text-center place-self-center transition">
-                        <Link href={"/"}>
+                        <Link href={"/product/17fd17d1-12d9-49dd-bc4e-c019f5f7f3fe"}>
                                 Discover
                         </Link>
                     </button>
